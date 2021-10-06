0f040b77f0fcfc7049c242b6b2bbd74bd2479c5b
zen.com <ulel2123@gmail.com>
2021-10-06 18:28:39 +0700

    Sect 1 72%, Sect 2 10%

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div>
       <div className = 'z-10 right-0 fixed h-full' ref = {bg1Ref} style = {{backgroundImage: 'url("images/sec-1-2-bg-transisi.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
-      <div className = 'z-15 right-0 fixed h-full sm:h-1/2' ref = {gapuraRef} style = {{backgroundImage: 'url("images/gapura-sec1-2.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
+      <div className = 'z-15 right-0 fixed h-full' ref = {gapuraRef} style = {{backgroundImage: 'url("images/gapura-sec1-2.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
       {/* <div className = 'z-20 right-0 absolute' ref = {bg2Ref} style = {{height: "100vh", width: "100vw", backgroundImage: 'url("images/Group bg 2.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div> */}
       <img className = 'z-0 absolute' src = { ("images/kolase foto.svg") } alt = 'Kolase Foto Dokumentasi Porsenigama' />            
       <Section1 onClick = {onClick} />
