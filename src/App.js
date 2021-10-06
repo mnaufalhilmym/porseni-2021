15ef23507f6f7cc1888b9849d17751e5eef61321
Irfan Azhar <muhammad.irfan.azhar@mail.ugm.ac.id>
2021-10-06 18:32:46 +0700

    initiate footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { useRef, useEffect } from 'react'
-import Section1 from './components/Section1'
-import Section2 from './components/Section2'
+import React, { useRef, useEffect } from "react";
+import Section1 from "./components/Section1";
+import Section2 from "./components/Section2";
+import Footer from "./components/Footer";
 
 function App() {
   const bg1Ref = useRef();
@@ -9,26 +10,62 @@ function App() {
 
   useEffect(() => {
     const onScroll = (event) => {
-      bg1Ref.current.style.width = window.innerWidth*(8/10) + window.pageYOffset + "px"
-      gapuraRef.current.style.width = window.innerWidth*(4/10) + window.pageYOffset*(3/2) + "px"
+      bg1Ref.current.style.width =
+        window.innerWidth * (8 / 10) + window.pageYOffset + "px";
+      gapuraRef.current.style.width =
+        window.innerWidth * (4 / 10) + window.pageYOffset * (3 / 2) + "px";
     };
-    
-    window.addEventListener('scroll', onScroll);
-    
+
+    window.addEventListener("scroll", onScroll);
   }, []);
 
   const onClick = () => {
-    document.getElementById('Section2').scrollIntoView({behavior: 'smooth'})
+    document.getElementById("Section2").scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <div>
-      <div className = 'z-10 right-0 fixed' ref = {bg1Ref} style = {{height: "100vh", width: "80vw", backgroundImage: 'url("images/Group bg 1.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
-      <div className = 'z-15 right-0 fixed' ref = {gapuraRef} style = {{height: "100vh", width: "40vw", backgroundImage: 'url("images/gapura-sec1-2.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
-      <div className = 'z-20 right-0 absolute' ref = {bg2Ref} style = {{height: "100vh", width: "100vw", backgroundImage: 'url("images/Group bg 2.svg")', backgroundRepeat: "no-repeat", backgroundSize: "auto"}}></div>
-      <img className = 'z-0 absolute' src = { ("images/kolase foto.svg") } alt = 'Kolase Foto Dokumentasi Porsenigama' />            
-      <Section1 onClick = {onClick} />
+      <div
+        className="z-10 right-0 fixed"
+        ref={bg1Ref}
+        style={{
+          height: "100vh",
+          width: "80vw",
+          backgroundImage: 'url("images/Group bg 1.svg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "auto",
+        }}
+      ></div>
+      <div
+        className="z-15 right-0 fixed"
+        ref={gapuraRef}
+        style={{
+          height: "100vh",
+          width: "40vw",
+          backgroundImage: 'url("images/gapura-sec1-2.svg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "auto",
+        }}
+      ></div>
+      <div
+        className="z-20 right-0 absolute"
+        ref={bg2Ref}
+        style={{
+          height: "100vh",
+          width: "100vw",
+          backgroundImage: 'url("images/Group bg 2.svg")',
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "auto",
+        }}
+      ></div>
+      <img
+        className="z-0 absolute"
+        src={"images/kolase foto.svg"}
+        alt="Kolase Foto Dokumentasi Porsenigama"
+      />
+      <Section1 onClick={onClick} />
       <Section2 />
+      <Footer />
     </div>
   );
 }
