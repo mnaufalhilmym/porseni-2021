46fb221d0fbe052121c90c32697560e87c9424c2
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2021-10-09 08:28:45 +0700

    add images jejepangan vertikal and figur

--- src/components/Section1.js
+++ src/components/Section1.js
@@ -1,21 +1,24 @@
-import Title from './Title/Title.js'
-import Side1 from './Title/Side1.js'
+import Title from "./Title/Title.js";
+import Side1 from "./Title/Side1.js";
 
 const Section1 = (props) => {
+  return (
+    <div className="w-full">
+      <div className="mt-0 min-h-screen flex flex-col md:flex-row md:content-end md:justify-end relative">
+        <Title onClick={props.onClick} />
+        <Side1 />
+      </div>
+      <div className="h-6"></div>
+      <div className="md:hidden flex relative justify-center">
+        <img
+          onClick={props.onClick}
+          className="z-20 cursor-pointer transform transition duration-300 active:scale-125"
+          src={"images/Polygon 1.svg"}
+          alt="button to scroll"
+        />
+      </div>
+    </div>
+  );
+};
 
-
-    return (
-        <div className = 'w-full'>
-            <div className = 'mt-0 min-h-screen flex flex-col md:flex-row md:content-end md:justify-end relative'>
-                <Title onClick = {props.onClick}/>
-                <Side1 />
-            </div>
-            <div className = 'h-6'></div>
-            <div className = 'md:hidden flex relative justify-center'>
-                <img onClick = {props.onClick} className = 'z-20 cursor-pointer transform transition duration-300 active:scale-125' src = { ("images/Polygon 1.svg") } alt = "button to scroll" />
-            </div>
-        </div>
-    )
-}
-
-export default Section1
+export default Section1;
--- src/components/Section2.js
+++ src/components/Section2.js
@@ -4,37 +4,45 @@ const Section2 = () => {
   return (
     <div
       id="Section2"
-      className="relative z-30 min-h-screen w-full rounded-full font-nuku text-4xl"
+      className="relative z-30 pt-16 min-h-screen w-full rounded-full font-nuku text-4xl"
     >
-      <div className="py-16">
-        <Timeline />
-        <div className="mx-auto flex flex-col xl:flex-row justify-center items-center">
-          <button
-            className="w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
-            style={{
-              backgroundImage: "url('/images/button.svg')",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-            }}
-            onClick={() => window.open('#','_blank')}
-          >
-            DAFTAR
-          </button>
-          <button
-            className="w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
-            style={{
-              backgroundImage: "url('/images/button.svg')",
-              backgroundRepeat: "no-repeat",
-              backgroundSize: "contain",
-              backgroundPosition: "center",
-            }}
-            onClick={() => window.open('#','_blank')}
-          >
-            GUIDEBOOK
-          </button>
-        </div>
+      <img
+        className="absolute right-20"
+        src="/images/jejepangan-vertikal.svg"
+        alt="jejepangan vertikal"
+      ></img>
+      <Timeline />
+      <div className="mx-auto flex flex-col xl:flex-row justify-center items-center">
+        <button
+          className="w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
+          style={{
+            backgroundImage: "url('/images/button.svg')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+          }}
+          onClick={() => window.open("#", "_blank")}
+        >
+          DAFTAR
+        </button>
+        <button
+          className="z-10 w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
+          style={{
+            backgroundImage: "url('/images/button.svg')",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "contain",
+            backgroundPosition: "center",
+          }}
+          onClick={() => window.open("#", "_blank")}
+        >
+          GUIDEBOOK
+        </button>
       </div>
+      <img
+        className="relative ml-auto -mt-80"
+        src="/images/figur-cs.svg"
+        alt="figur"
+      ></img>
     </div>
   );
 };
