a3e9aeced2096d8434f136115b006833e317aa89
Muhammad Naufal Hilmy Makarim <mail@hilmy.dev>
2021-10-09 08:42:50 +0700

    add responsiveness in section 2

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -7,14 +7,14 @@ const Section2 = () => {
       className="relative z-30 pt-16 min-h-screen w-full rounded-full font-nuku text-4xl"
     >
       <img
-        className="absolute right-20"
+        className="absolute right-0 sm:right-20 bottom-20 xl:top-0"
         src="/images/jejepangan-vertikal.svg"
         alt="jejepangan vertikal"
       ></img>
       <Timeline />
-      <div className="mx-auto flex flex-col xl:flex-row justify-center items-center">
+      <div className="mx-auto flex flex-col lg:flex-row justify-center items-center">
         <button
-          className="w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
+          className="z-10 w-56 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-5xl uppercase"
           style={{
             backgroundImage: "url('/images/button.svg')",
             backgroundRepeat: "no-repeat",
@@ -26,7 +26,7 @@ const Section2 = () => {
           DAFTAR
         </button>
         <button
-          className="z-10 w-60 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-fiftysix uppercase"
+          className="z-10 w-56 h-24 sm:w-96 sm:h-32 my-3 sm:my-8 mx-10 sm:text-5xl uppercase"
           style={{
             backgroundImage: "url('/images/button.svg')",
             backgroundRepeat: "no-repeat",
@@ -39,7 +39,7 @@ const Section2 = () => {
         </button>
       </div>
       <img
-        className="relative ml-auto -mt-80"
+        className="relative xl:ml-auto -mt-80 w-96 xl:w-max"
         src="/images/figur-cs.svg"
         alt="figur"
       ></img>
